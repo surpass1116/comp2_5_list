73e36ca3a9c2e3b9e3f669a85e236b119855a147
imagire <imagire@gmail.com>
2024-11-03 16:24:10 +0900

    add files

---
+++ src/StaticLib/StaticLib.c
@@ -0,0 +1,41 @@
+﻿#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
+#include "Windows.h"                    // Windows API の機能定義
+
+#include "../include/lib_func.h"
+
+
+// ノードを初期化
+void initialize_node(node* p, int val)
+{
+	p->pNext = NULL;
+	p->data = val;
+}
+
+// リストを初期化
+void initialize_list(list* l)
+{
+	l->header = NULL;
+}
+
+
+// 先頭にデータを追加
+void push_front(list* l, node* p)
+{
+	// ToDo: ここにコードを追加
+}
+
+// pの次のノードを削除
+void remove_next(list* l, node* p)
+{
+	// ToDo: ここにコードを追加
+}
+
+
+// pの次のノードを取得
+node* get_next(list* l, node* p)
+{
+	if (!p) return NULL;
+	return p->pNext;
+
+	return NULL;
+}
